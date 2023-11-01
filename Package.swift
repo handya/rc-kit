87cc7e0c56512ebf3b5c6f4096ccde5f9a2e9aeb
Andrew Farquharson <andrewfarquharson@Now-We-Try-It-My-Way.local>
2023-11-02 12:15:45 +1300

    update package

--- Package.swift
+++ Package.swift
@@ -6,18 +6,29 @@ import PackageDescription
 let package = Package(
     name: "rc-kit",
     platforms: [
-       .macOS(.v12)
+       .macOS(.v10_15)
     ],
     products: [
-        .library(name: "rc-kit", targets: ["rc-kit"]),
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "RCKit",
+            targets: ["RCKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
     ],
     targets: [
-        .target( name: "rc-kit", dependencies: [
-                .product(name: "Vapor", package: "vapor")
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "RCKit",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
             ]),
-        .testTarget(name: "RCKitTests", dependencies: ["rc-kit"]),
+        .testTarget(
+            name: "RCKitTests",
+            dependencies: [
+                .target(name: "RCKit"),
+        ]),
     ]
 )
